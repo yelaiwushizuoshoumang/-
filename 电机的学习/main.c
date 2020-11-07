9d269a8ff0c1ace68a1becb2b81a95b2d3d053b1
Ars Nove <n@qq.com>
2020-11-07 17:02:32 +0800

    多个电机的调试

--- 电机的学习/main.c
+++ 电机的学习/main.c
@@ -3,21 +3,21 @@
 #include "delay.h"
 #include "PID.h"
 
-#define M3508_SPEED_PID_KP 10//15000.0f
+#define M3508_SPEED_PID_KP 0.0//15000.0f
 #define M3508_SPEED_PID_KI 0.0f//10.0f
 #define M3508_SPEED_PID_KD 0.0f
 #define M3508_SPEED_PID_MAX_OUT MAX_CAN_CURRENT
 #define M3508_SPEED_PID_MAX_IOUT 2000.0f
 #define MAX_CAN_CURRENT 16000.0f //最大电流
 
-uint16_t out=0;
-int16_t rpm_set=100;//每分钟转速设置期望
+int16_t rpm_set_01=0;//每分钟转速设置期望
+int16_t rpm_set_04=0;//每分钟转速设置期望
 
-const static fp32 M3508_speed_pid[3] = {M3508_SPEED_PID_KP, M3508_SPEED_PID_KI, M3508_SPEED_PID_KD};
+pid_type_def PID_3508[4];//数组0-->电调1，数组3-->对应电调4
 
-pid_type_def PID_3508;
+const static fp32 M3508_speed_pid[4] = {M3508_SPEED_PID_KP, M3508_SPEED_PID_KI, M3508_SPEED_PID_KD};
 
-extern motor_measure_t M3508_Set;//结构体
+extern motor_measure_t M3508_Set[4];//结构体
 
 int main(void)
 {
@@ -26,14 +26,19 @@ int main(void)
 	Can_Init();
 	LED_Init();
 	LED1=~LED1;
-	PID_init(&PID_3508,PID_POSITION,M3508_speed_pid, M3508_SPEED_PID_MAX_OUT, M3508_SPEED_PID_MAX_IOUT);
+	PID_init(&PID_3508[0],PID_POSITION,M3508_speed_pid, M3508_SPEED_PID_MAX_OUT, M3508_SPEED_PID_MAX_IOUT);
+	
+	PID_init(&PID_3508[3],PID_POSITION,M3508_speed_pid, M3508_SPEED_PID_MAX_OUT, M3508_SPEED_PID_MAX_IOUT);
 	while(1)
 	{
-	PID_calc(&PID_3508,M3508_Set.speed_rpm,rpm_set);//PID的计算，速度
-	CAN_CMD_CHASSIS(PID_3508.out,PID_3508.out,0,0); 
 		
+	PID_calc(&PID_3508[0],M3508_Set[0].speed_rpm,rpm_set_01);//PID的计算，速度
+	PID_calc(&PID_3508[3],M3508_Set[3].speed_rpm,rpm_set_04);//PID的计算，速度
+	
+	CAN_CMD_CHASSIS(PID_3508[0].out,0,0,PID_3508[3].out); 
 	delay_ms(1);
 	LED0 = ~LED0;
+		
 	}
 }
 	
