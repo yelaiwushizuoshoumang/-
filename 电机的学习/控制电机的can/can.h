020184d51081d5b3e2deb357481c597920598da4
Ars Nove <n@qq.com>
2020-11-07 17:04:48 +0800

    加入PID（多电机调试）

--- 电机的学习/控制电机的can/can.h
+++ 电机的学习/控制电机的can/can.h
@@ -16,7 +16,39 @@
 #define ADVANCE_CAN_BS2   CAN_BS2_8tq
 #define ADVANCE_CAN_Fdiv  2
 //==============================================
+typedef struct
+{
+    uint16_t ecd;
+    int16_t speed_rpm;
+    int16_t given_current;
+    int32_t  all_ecd;
+    int32_t  count;
+    uint8_t temperate;
+    int16_t last_ecd;
+} motor_measure_t;//电机的测量值
 
+//===========电调ID设置=========================
+typedef enum
+{
+    CAN_3508_CHASSIS_ALL_ID =0X200,	
+    CAN_3508_M1_ID = 0x201,
+    CAN_3508_M2_ID = 0x202,
+    CAN_3508_M3_ID = 0x203,
+    CAN_3508_M4_ID = 0x204,
+	
+}MC620_ID_Set;
+//===============================================
+#define get_motor_measure(ptr, rx_message)                                              \
+{                                                                                       \
+    if((ptr)->ecd - (ptr)->last_ecd > 4096) (ptr)->count-- ;                            \
+	else if((ptr)->ecd - (ptr)->last_ecd < -4096 ) (ptr)->count ++ ;                    \
+    (ptr)->last_ecd = (ptr)->ecd;                                                       \
+    (ptr)->ecd = (uint16_t)((rx_message).Data[0] << 8 | (rx_message).Data[1]);          \
+    (ptr)->speed_rpm = (uint16_t)((rx_message).Data[2] << 8 |(rx_message).Data[3]);     \
+    (ptr)->given_current = (uint16_t)((rx_message).Data[4] << 8 | (rx_message).Data[5]); \
+    (ptr)->temperate = (rx_message).Data[6];                                             \
+    (ptr)->all_ecd=(ptr)->count*8191+(ptr)->ecd;                                     \
+}
 //=========================
 void Can_Init_Mode(void);
 void Can_Init(void);
